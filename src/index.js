8399a84060a0bc5e26e8cc60f5e57152b49d4817
elisa.zyj <elisa.zyj@link8tech.tw>
2021-09-01 13:56:59 +0800

    feat([create]): 創建專案
    引入msw

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+import { setupMSW } from './mocks/browser'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// setupMSW().then(() => 初始化APP)
+setupMSW().then(() =>
+  ReactDOM.render(<App />, document.getElementById('root'))
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
